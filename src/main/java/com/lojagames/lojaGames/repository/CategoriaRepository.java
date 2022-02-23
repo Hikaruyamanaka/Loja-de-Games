25d888907f1cf81827d42a87ad99e5037af4b3a0
Rafael Casotti <rafa_scasotti@hotmail.com>
2022-02-23 11:29:44 -0300

    @RafaelCasotti [Task: CREATE categoria repositorio]

---
+++ src/main/java/com/lojagames/lojaGames/repository/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.lojagames.lojaGames.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.lojagames.lojaGames.models.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+	public List<Categoria> findAllByTipoContainingIgnoreCase(String tipo);
+
+}
