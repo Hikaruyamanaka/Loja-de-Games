fe3bf71f6bf7ea49f38e6b51aea1e368442055b6
sotie <javacursobustos@gmail.com>
2022-02-23 15:19:03 -0300

    @bustos00 [Task: Criando controller Id]

--- src/main/java/com/lojagames/lojaGames/controller/ProdutoController.java
+++ src/main/java/com/lojagames/lojaGames/controller/ProdutoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,17 +14,23 @@ import com.lojagames.lojaGames.models.Produto;
 import com.lojagames.lojaGames.repository.ProdutoRepository;
 
 @RestController
-@CrossOrigin (origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/produtos")
 public class ProdutoController {
-	
+
 	@Autowired
 	private ProdutoRepository repository;
 
 	@GetMapping
-	public ResponseEntity<List<Produto>> getAll(){
+	public ResponseEntity<List<Produto>> getAll() {
 		return ResponseEntity.ok(repository.findAll());
-		
 	}
-		
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Produto> GetById(@PathVariable(value = "id") long id) {
+
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+
 }
