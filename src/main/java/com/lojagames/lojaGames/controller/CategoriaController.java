ad7abe4e9eb1b07431f210d0ff757516101b77df
Anita <falchi.anita@gmail.com>
2022-02-23 12:03:44 -0300

    @anita-falchi: [Task: Criando endPoint FindAll Categoria]

--- src/main/java/com/lojagames/lojaGames/controller/CategoriaController.java
+++ src/main/java/com/lojagames/lojaGames/controller/CategoriaController.java
@@ -1,10 +1,15 @@
 package com.lojagames.lojaGames.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lojagames.lojaGames.models.Categoria;
 import com.lojagames.lojaGames.repository.CategoriaRepository;
 
 @RestController
@@ -14,4 +19,11 @@ public class CategoriaController {
 
 	@Autowired
 	 private CategoriaRepository repository ;
+	
+	@GetMapping
+	public ResponseEntity<List<Categoria>> GetAll (){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	
 }
