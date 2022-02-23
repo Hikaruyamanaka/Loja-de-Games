76912050e3817e436be418764bff0005349752bd
unknown <samara_alves28@hotmail.com>
2022-02-23 15:04:02 -0300

    @Samara-Alves[Task:criando camada de produto controller]

---
+++ src/main/java/com/lojagames/lojaGames/controller/ProdutoController.java
@@ -0,0 +1,29 @@
+package com.lojagames.lojaGames.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lojagames.lojaGames.models.Produto;
+import com.lojagames.lojaGames.repository.ProdutoRepository;
+
+@RestController
+@CrossOrigin (origins = "*", allowedHeaders = "*")
+@RequestMapping("/produtos")
+public class ProdutoController {
+	
+	@Autowired
+	private ProdutoRepository repository;
+
+	@GetMapping
+	public ResponseEntity<List<Produto>> getAll(){
+		return ResponseEntity.ok(repository.findAll());
+		
+	}
+		
+}
