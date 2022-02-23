5260973ddb792f27dbce3b2e4e3f7cc108f839d3
Fabiola <fabiolasou.za@live.com>
2022-02-23 14:36:10 -0300

    @fabiolasoufer [Task: CREATE model Produto e Relacionamento de tabelas]

--- src/main/java/com/lojagames/lojaGames/models/Categoria.java
+++ src/main/java/com/lojagames/lojaGames/models/Categoria.java
@@ -1,12 +1,18 @@
 package com.lojagames.lojaGames.models;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_categoria")
@@ -19,6 +25,23 @@ public class Categoria {
 	@NotNull
 	@Size(min = 10, max = 500)
 	private String descricao;
+	
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
+	
+	private List<Produto> produto;
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 
 	public long getId() {
 		return id;
