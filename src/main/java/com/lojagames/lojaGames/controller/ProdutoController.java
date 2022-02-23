72106242dfe607753e4d6425150bda54519aae17
Hikaru <hikaru.yamanaka@outlook.com>
2022-02-23 15:48:33 -0300

    @HikaruYamanaka [Task: Finalizando CRUD de Produto]

--- src/main/java/com/lojagames/lojaGames/controller/ProdutoController.java
+++ src/main/java/com/lojagames/lojaGames/controller/ProdutoController.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,4 +48,23 @@ public class ProdutoController {
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(repository.save(produto));
 	}
+	
+	@PutMapping
+	public ResponseEntity<Produto> put (@RequestBody Produto produto){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(produto));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id)
+	{
+		repository.deleteById(id);
+	}
+	
 }
+
+
+
+
+
+
+
